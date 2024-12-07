05f616bbe392bd63bbd21b318ce86edd63d77b08
Robert <robert113289@gmail.com>
2024-12-07 11:08:59 -0800

    create project with spring initializer

---
+++ src/test/java/com/cribl/logcollector/LogCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cribl.logcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
